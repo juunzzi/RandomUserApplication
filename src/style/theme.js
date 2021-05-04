17897f0daaceffdd120bc8b9f20b993c6a8bf920
junzzi <jjh01027717502@gmail.com>
2021-05-04 21:42:27 +0900

    feature: add src/style/global.js & theme.js

---
+++ src/style/theme.js
@@ -0,0 +1,10 @@
+const size = {
+  mobile: "800px",
+  desktop: "1920px",
+};
+const theme = {
+  mainColor: "#white",
+  mobile: `(max-width:${size.mobile})`,
+  desktop: `(max-width:${size.desktop})`,
+};
+export default theme;
