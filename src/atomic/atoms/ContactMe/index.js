4133b50832577042ac8d2be0d33b66d263b857ec
junzzi <jjh01027717502@gmail.com>
2021-05-10 20:11:54 +0900

    feature: update atmos/ContactMe

---
+++ src/atomic/atoms/ContactMe/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+const Text = styled.span`
+  font-size: 20px;
+  font-weight: 500;
+  color: rgba(0, 0, 0, 0.7);
+`;
+function ContactMe() {
+  return <Text>🕶 Contact Me 🕶</Text>;
+}
+
+export default ContactMe;
