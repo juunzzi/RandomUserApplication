59c148de6b62af14cc66bfdfef542ffae755ce62
junzzi <jjh01027717502@gmail.com>
2021-05-10 20:11:41 +0900

    feature: add atoms/AboutMe

---
+++ src/atomic/atoms/AboutMe/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+const Text = styled.span`
+  font-size: 20px;
+  font-weight: 500;
+  color: rgba(0, 0, 0, 0.7);
+`;
+
+function AboutMe() {
+  return <Text>😁 About Me 😁</Text>;
+}
+
+export default AboutMe;
