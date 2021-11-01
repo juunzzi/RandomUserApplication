3942192470f9309710b7e2edae352fa4402d20ae
junzzi <jjh01027717502@gmail.com>
2021-11-01 19:35:33 +0900

    feature: Landing 페이지 개발

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FixedSizeList as List } from "react-window";
 import styled from "styled-components";
-import ExploreContent from "../../components/LandingPage/ExploreContent";
-import VelogContent from "../../components/LandingPage/VelogContent";
-const Wrapper = styled.div``;
+import LinkButton from "../../components/common/LinkButton";
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100vh;
+
+  & > div:not(div:last-child) {
+    margin-right: 30px;
+  }
+`;
+
+const data = [...new Array(100)].map(() => ({ text: "124124124124" }));
 const LandingPage = () => {
   return (
     <Wrapper>
-      <ExploreContent></ExploreContent>
-      <VelogContent></VelogContent>
+      <LinkButton to="/create" text="생성"></LinkButton>
+      <LinkButton to="/list" text="리스트"></LinkButton>
+      <LinkButton to="/list" text="리스트"></LinkButton>
     </Wrapper>
   );
 };
+
 export default LandingPage;
