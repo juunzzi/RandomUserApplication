73c074ae0e8ff5bf41b97d8f813d370aa03aac67
junzzi <jjh01027717502@gmail.com>
2021-11-01 19:33:29 +0900

    feature: 반응형 훅 & 전역 변수 추가

---
+++ src/hooks/useResponsive.js
@@ -0,0 +1,15 @@
+import { useEffect, useLayoutEffect, useState } from "react";
+
+const useResponsive = () => {
+  const [size, setSize] = useState(null);
+  useEffect(() => {
+    const updateSize = () => {
+      setSize(window.innerWidth);
+    };
+    updateSize();
+    window.addEventListener("resize", updateSize);
+  }, []);
+
+  return size;
+};
+export default useResponsive;
--- src/style/theme.js
+++ src/style/theme.js
@@ -1,10 +1,13 @@
 const size = {
-  mobile: "800px",
-  desktop: "1920px",
+  mobile: "480px",
+  tablet: "600px",
+  desktop: "1024px",
 };
+
 const theme = {
   mainColor: "#ffffff",
-  mobile: `(max-width:${size.mobile})`,
-  desktop: `(max-width:${size.desktop})`,
+  mobile: `@media (max-width: ${size.mobile})`,
+  tablet: `@media (min-width: ${size.tablet})`,
+  desktop: `@media (min-width: ${size.desktop})`,
 };
 export default theme;
